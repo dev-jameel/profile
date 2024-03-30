d81deee21978e94a5ab377dd851c64e15ab4585d
Jameel Shaikh <shaikhjameel17@gmail.com>
2024-03-30 16:42:22 +0530

    Update Navbar.jsx

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -32,11 +32,11 @@ const Navbar = () => {
                 </div>
                 <div className="navbar-menu" ref={menuToggle}>
                     <ul className="navbar-menu-list">
-                        <li><a href="#home">Home</a></li>
-                        <li><a href="#about">About</a></li>
-                        <li><a href="#services">Services</a></li>
-                        <li><a href="#portfolio">Portfolio</a></li>
-                        <li><a href="#contact">Contact</a></li>
+                        <li><a href="#home" onClick={toggleMenu}>Home</a></li>
+                        <li><a href="#about" onClick={toggleMenu}>About</a></li>
+                        <li><a href="#services" onClick={toggleMenu}>Services</a></li>
+                        <li><a href="#portfolio" onClick={toggleMenu}>Portfolio</a></li>
+                        <li><a href="#contact" onClick={toggleMenu}>Contact</a></li>
                     </ul>
                     <div className="contact-btn">
                         <a href="#contact">Get In Touch</a>
